f7d0d8905041058d6cfca158def8e18a1556c99c
sudagarw <sudagarw@cisco.com>
2017-07-10 20:34:33 +0000

    Add format checking validation.

--- csap-cron/poll-canvas.py
+++ csap-cron/poll-canvas.py
@@ -15,12 +15,18 @@ def threshold(days=0, hours=0, minutes=0, seconds=0):
     return days*24*3600 + hours*3600 + minutes*60 + seconds
 
 DEFAULT_THRESHOLD = threshold(hours=6)
-canvas_token = '9881~NaOD1DvHAF1mFhmyORwMkNzmUb7saHTHUQKmvexWdnEFoRmTQ3QuvVuqvuGnacKk'
-base_url = 'https://ciscoacademy.test.instructure.com'
+old_canvas_token = '9881~NaOD1DvHAF1mFhmyORwMkNzmUb7saHTHUQKmvexWdnEFoRmTQ3QuvVuqvuGnacKk'
+canvas_token = '9881~2QiAZJEE1abpIwZ58N5Gufn4cl2qXuRdWZFfOzPX4HII5NHxuFQKHZg1X3sDgrO0'
+base_url = 'https://ciscoacademy.instructure.com'
 api_prefix = '/api/v1'
 canvas = CanvasReader(canvas_token, base_url, api_prefix, verbose=False)
 spark_token = 'OGM5YTM4NjYtYWIzZC00YzcwLThhZmMtZmEwOTZlZGRhNTU0YzNhMDQ4YjAtNWEx'
 spark = CiscoSparkAPI(access_token=spark_token)
+
+roles = ['ASR', 'ASE', 'ASX']
+locations = ['RTP', 'SJC', 'EMEAR', 'AMS I', 'AMS II', 'SNG', 'APJC', 'Americas', 'EMEAR II', 'AMS', 'APJC']
+asx_locations = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5', 'Team 6', 'Team 7', 'Team 8', 'Team 9', 'Team 10', 'Team 11']
+
 
 def user_in_team(user_id, team_id):
     member_ids = [membership.personEmail for membership in spark.team_memberships.list(teamId=team_id)]
@@ -67,14 +73,59 @@ def create_room_for_section(section):
 
 subaccount_name = 'FY18Q1 Cisco Sales Associates Program (CSAP)'
 def find_sections():
+    instructors = pd.read_csv('/home/ec2-user/csap/csap-data/instructors.csv')
     res = []
     log = '*Polling Canvas for upcoming sections . . .*<br><br>'
+    log += 'The following sections are formatted differently on Canvas. Logs are printed in the format `section_name`, `course_name`, `role`, `location`, `instructor`.<br><br>'
     for course in canvas.get_courses_for_subaccount(subaccount_name):
         course_id = course['id']
         sections = canvas.get_sections(course_id)
         for section in sections:
             start = section['start_at']
             if start:
+
+                location = None
+                role = None
+                instructor = None
+                
+                # Find role
+                for r in roles:
+                    if r in course['name']:
+                        role = r
+                        
+                # Find location
+                if role == 'ASX':
+                    for l in asx_locations:
+                        if l in section['name']:
+                            location = l
+                else:
+                    for l in locations:
+                        if l in section['name']:
+                            location = l
+                        
+                # Find instructor
+                if '-' in section['name']:
+                    instructor = section['name'].split('-')[-1].strip()
+                    name = instructor.split(' ')
+                    query = instructors[(instructors.first_name == name[0]) & (instructors.last_name == name[1])].user_id
+                    if len(query) == 1:
+                        instructor = query.item()
+
+                if not role or not location or not instructor:
+                    log += '%s, %s, ' % (section['name'], course['name'])
+                    if role:
+                        log += '%s, ' % role
+                    else:
+                        log += '**%s, **' % role
+                    if location:
+                        log += '%s, ' % location
+                    else:
+                        log += '**%s, **' % location
+                    if instructor:
+                        log += '%s' % instructor
+                    else:
+                        log += '**%s**<br>' % instructor
+
                 tz = timezone(course['time_zone'])
                 dt = dateutil.parser.parse(start, tzinfos=[tz])
                 ts = (dt - datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds()
@@ -83,16 +134,19 @@ def find_sections():
                 if diff > 0 and diff < DEFAULT_THRESHOLD:
                     section['course_name'] = course['name']
                     section['course_tz'] = course['time_zone']
+                    section['start_unix'] = ts
                     res.append(section)
-                    log += str(tz) + '<br>'
-                    log += str(start) + '<br>'
-                    log += '**The %s section for %s starts in %f hours.**<br><br>' % (section['name'], section['course_name'], diff/3600)
-    if len(res) == 0:
-        log += 'There are no sections starting soon.'
     return (res, log)
 
 sections, log = find_sections()
+if len(sections) == 0:
+    log += '<br>There are no sections starting soon.'
+else:
+    for section in sections:
+        print(section)
+        log += '<br>**The %s section for %s starts in %f hours.**<br>' % (section['name'], section['course_name'], (section['start_unix']-time.time())/3600)
 print(log)
+
 created = pd.read_csv('/home/ec2-user/csap/csap-data/created.csv')
 for section in sections:
     if section['id'] in list(created.id):
